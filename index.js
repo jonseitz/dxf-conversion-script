a169040fd524b5d128378c5aea5ba828fd108df0
Jonathan Seitz <seitz.jon@gmail.com>
2018-10-21 23:35:11 -0400

    break svgs out into layers

--- index.js
+++ index.js
@@ -4,10 +4,8 @@ import { xml2json } from "xml-js";
 import { resolve } from "path";
 
 const layers = [ 
-//  'A-WALL-INTR',
   'A-FLOR-EVTR',
   'A-FLOR-TPTN',
-//  'A-FLOR-HRAL',
   'A-FLOR-STRS',
   'EC1 Area Polygons', 
   'P-FIXT',
@@ -18,39 +16,50 @@ const layers = [
   fs.promises.readdir("./dxf")
   .then(async (paths) => {
     return await paths.forEach(async (path) => {
+      if (!/\.dxf$/.test(path)) {
+        return;
+      }
       const fullPath = resolve(__dirname, "dxf", path);
       console.log(`opening ${fullPath}`);
       const file = await fs.promises.open(fullPath, 'r');
       //grab file contents
       const content = await file.readFile({encoding: "UTF-8"});
       await file.close();
-      let parsed = dxf.parseString(content);
       //loop through entities, removing those from unneeded layers
-      let newEntities = [];
-      parsed.entities.forEach(entity => {
-        if (layers.includes(entity.layer)) {
-          newEntities.push(entity);
-        }
-      })
-      parsed.entities = newEntities;  
-      //write an svg version of the file
-      const svgName = path.replace(/\.dxf$/, "-mod.svg");
-      const svgPath = resolve(__dirname, "svg", svgName); 
-      console.log(`writing svg file to ${svgName}`);
-      const svgFile = await fs.promises.open(svgPath, 'w');
-      const svgVersion = dxf.toSVG(parsed);
-      await svgFile.writeFile(svgVersion);
-      await svgFile.close();
-      console.log(`wrote ${svgPath}`)
-      //create a json representation of the svg data
-      const jsonName = path.replace(/\.dxf$/, "-mod.json");
-      const jsonPath = resolve(__dirname, "json", jsonName); 
-      console.log(`writing json file to ${jsonName}`);
-      const jsonFile = await fs.promises.open(jsonPath, 'w');
-      const jsonVersion = xml2json(svgVersion, {compact: true, spaces: 2});
-      await jsonFile.writeFile(jsonVersion);
-      console.log(`wrote ${jsonPath}`)
-      await jsonFile.close();
+      layers.forEach(async (layer) => {
+        let parsed = dxf.parseString(content);
+        let newEntities = [];
+        parsed.entities.forEach(entity => {
+          if (entity.layer === layer) {
+            newEntities.push(entity);
+          }
+        })
+        Object.keys(parsed.tables.layers).forEach(tableLayer => {
+          if (tableLayer !== layer) {
+           delete parsed.tables.layers[tableLayer];
+          }
+        })
+        parsed.entities = newEntities;  
+      
+        //write an svg version of the file
+        const svgName = path.replace(/\.dxf$/,`-${layer}.svg`);
+        const svgPath = resolve(__dirname, "svg", svgName); 
+        console.log(`writing svg file to ${svgName}`);
+        const svgFile = await fs.promises.open(svgPath, 'w');
+        const svgVersion = dxf.toSVG(parsed);
+        await svgFile.writeFile(svgVersion);
+        await svgFile.close();
+        console.log(`wrote ${svgPath}`)
+        //create a json representation of the svg data
+        const jsonName = path.replace(/\.dxf$/, `-${layer}.json`);
+        const jsonPath = resolve(__dirname, "json", jsonName); 
+        console.log(`writing json file to ${jsonName}`);
+        const jsonFile = await fs.promises.open(jsonPath, 'w');
+        const jsonVersion = xml2json(svgVersion, {compact: true, spaces: 2});
+        await jsonFile.writeFile(jsonVersion);
+        console.log(`wrote ${jsonPath}`)
+        await jsonFile.close();
+      });
     });
   })
 })()
